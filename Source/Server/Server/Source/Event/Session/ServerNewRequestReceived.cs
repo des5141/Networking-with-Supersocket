45c72a456a74ad4ab56a814d7128a2caf4816b90
des5141 <rhea31@naver.com>
2019-04-15 20:41:26 +0900

    Update ServerNewRequestReceived.cs

--- Source/Server/Server/Source/Event/Session/ServerNewRequestReceived.cs
+++ Source/Server/Server/Source/Event/Session/ServerNewRequestReceived.cs
@@ -7,14 +7,12 @@ namespace Networking_with_Supersocket
     {
         public static void func(NcsUser user, NcsRequestInfo requestInfo)
         {
-            for (int i = 0; i < requestInfo.Buffer.len; i++)
-                Console.WriteLine(requestInfo.Buffer.buf[i]);
             var buffer = requestInfo.Buffer;
             UInt32 bufferLength = buffer.extract_uint();
             ushort signal = buffer.extract_ushort();
             switch (signal)
             {
-                case Signal.signal_heartbeat_first:
+                case 1:
                     user.Send(NcsTemplateBuffer.HeartbeatBuffer2);
                     user.heartbeat = true;
                     //Encoding.UTF8.GetString(buffer.extract<byte[]>())
