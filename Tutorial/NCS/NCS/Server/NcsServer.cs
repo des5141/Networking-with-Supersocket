10dfeb4997ba5d2cc00d53bce02ef38912f0b2ad
Hot-key <rhanwnwmf@protonmail.ch>
2019-04-17 09:53:28 +0900

    Ncs 작업물 추가

---
+++ Tutorial/NCS/NCS/Server/NcsServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
+
+namespace Ncs.Server
+{
+    public class NcsServer : AppServer<NcsUser, NcsRequestInfo>
+    {
+        public NcsServer() : base(new DefaultReceiveFilterFactory<NcsReceiveFilter, NcsRequestInfo>())
+        {
+
+        }
+
+        protected override bool Setup(IRootConfig rootConfig, IServerConfig config)
+        {
+            return base.Setup(rootConfig, config);
+        }
+
+        protected override void OnStopped()
+        {
+            base.OnStopped();
+        }
+    }
+}
